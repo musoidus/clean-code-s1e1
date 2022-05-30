88af59b4fce22d3908e1b5e7b868808cf44370cb
Nina <nina.korocevic@icloud.com>
2022-05-30 12:59:05 +0300

    refactor: get rid of tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,19 +32,30 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.classList.add("list-item");
+    deleteButtonImg.classList.add("image");
+
     label.innerText=taskString;
-    label.className='task';
+    label.className="task";
+    label.classList.add("label");
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.classList.add("input");
+    checkBox.classList.add("checkbox-input");
     editInput.type="text";
     editInput.className="task";
+    editInput.classList.add("input");
+    editInput.classList.add("text-input");
+    
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
+    editButton.classList.add("button");
 
     deleteButton.className="delete";
-    deleteButtonImg.src='./remove.svg';
+    deleteButton.classList.add("button");
+    deleteButtonImg.src="./remove.svg";
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -82,7 +93,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector("input[type=text]");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
